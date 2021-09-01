34c849250834f9592b1fbfcf8c5364a676a19349
ulyanaprovalskaya <ulyana.provalskaya@gmail.com>
2021-09-01 13:20:54 +0300

    added BookingNotFoundException to exception handler

--- producer/src/main/java/eu/senla/ecabtask/producer/controller/exceptionhandler/ECabExceptionHandler.java
+++ producer/src/main/java/eu/senla/ecabtask/producer/controller/exceptionhandler/ECabExceptionHandler.java
@@ -2,15 +2,10 @@ package eu.senla.ecabtask.producer.controller.exceptionhandler;
 
 import eu.senla.ecabtask.exception.BookingNotFoundException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @ControllerAdvice
 @Slf4j
@@ -24,13 +19,8 @@ public class ECabExceptionHandler {
 
 
     @ExceptionHandler(BookingNotFoundException.class)
-    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    protected void handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpStatus status) {
-
-        List<String> errors = ex.getBindingResult()
-                .getFieldErrors()
-                .stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
-                .collect(Collectors.toList());
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    protected void handleMethodArgumentNotValid(BookingNotFoundException exception) {
+        log.error("BOOKING NOT FOUND: " + exception.getMessage());
     }
 }
